580d1dcc1de09659bd2084219bade598954f1581
Ekaterina Tebyakina <ekaterina.tebyakina@saritasa.com>
2024-07-15 09:51:45 +0700

    Fix PlayerResult type JC-632
    Co-authored-by: Violetta Klintsova <violetta.klintsova@saritasa.com>

--- apps/vanilla/src/pages/task2/core/models/player-result.ts
+++ apps/vanilla/src/pages/task2/core/models/player-result.ts
@@ -1,9 +1,9 @@
 /** Data about a dice roll and whether it became a winning one. */
-export type PlayerResult = {
+export type PlayerResult = Readonly<{
 
 	/** Win status. True if a player won. */
-	readonly winStatus: boolean;
+	winStatus: boolean;
 
 	/** Result of a dice roll. Number of points. */
-	readonly diceResult: number;
-};
+	diceResult: number;
+}>;
