dc397f3616716e99c6e499499c42bbf6def1336c
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-19 18:34:14 +0700

    Create api key interceptor JC-644

---
+++ apps/angular/src/core/interceptors/api-key.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { AppConfig } from '../utils/app-config';
+
+const appConfig = new AppConfig;
+
+/** Adds header 'Api-Key' to a request. */
+export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
+	const reqWithApiKey = req.clone({
+		headers: req.headers.set('Api-Key', appConfig.apiKey),
+	});
+  return next(reqWithApiKey);
+};
