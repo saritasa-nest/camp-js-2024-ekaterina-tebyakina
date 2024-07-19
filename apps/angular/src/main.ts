510fa27a2f4811e94d1ad84b03a635655cd4601d
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-19 18:32:42 +0700

    Setup providers JC-644

--- apps/angular/src/main.ts
+++ apps/angular/src/main.ts
@@ -1,19 +1,23 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor';
+import { apiKeyInterceptor } from './core/interceptors/api-key.interceptor';
 
 if (environment.production) {
 	enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-	providers: [provideRouter(appRoutes), provideAnimationsAsync(), provideHttpClient()],
+	providers: [
+		provideRouter(appRoutes),
+		provideAnimationsAsync(),
+		provideHttpClient(withInterceptors([baseUrlInterceptor, apiKeyInterceptor]), withFetch()),
+	],
 }).catch(err => console.error(err));
