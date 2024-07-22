eef27ae94b791ebab452ddbfb20fa3f21618e545
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-22 16:23:05 +0700

    Fix JSDoc comment JC-644

--- apps/angular/src/core/interceptors/api-key.interceptor.ts
+++ apps/angular/src/core/interceptors/api-key.interceptor.ts
@@ -3,7 +3,12 @@ import { AppConfig } from '../utils/app-config';
 
 const appConfig = new AppConfig;
 
-/** Adds header Api-Key to a request. */
+/**
+ * Adds header Api-Key to a request.
+ * @param req - Request.
+ * @param next - Request handler function.
+ * @returns Request with Api-Key.
+ */
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
 	const reqWithApiKey = req.clone({
 		headers: req.headers.set('Api-Key', appConfig.apiKey),
--- apps/angular/src/core/interceptors/base-url.interceptor.ts
+++ apps/angular/src/core/interceptors/base-url.interceptor.ts
@@ -3,7 +3,12 @@ import { AppConfig } from '../utils/app-config';
 
 const appConfig = new AppConfig;
 
-/** Adds base url to a request. */
+/**
+ * Adds base url to a request.
+ * @param req - Request.
+ * @param next - Request handler function.
+ * @returns Request with base url.
+ */
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
 	const reqWithBaseUrl = req.clone({url: `${appConfig.baseApiURL}/${req.url}`});
   return next(reqWithBaseUrl);
