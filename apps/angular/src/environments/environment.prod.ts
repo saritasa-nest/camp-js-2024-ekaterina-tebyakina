ebb05b8308e6ef64bd5218def687a9626d4fdf79
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-22 16:13:42 +0700

    Add environment variables JC-644

--- apps/angular/src/environments/environment.prod.ts
+++ apps/angular/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 /** Production environment configuration. */
 export const environment = {
 	production: true,
+	baseApiURL: 'https://api.camp-js.saritasa.rocks/api/v1',
+	apiKey: '4d114755-959f-4864-b484-b1752f9ae529',
 };
