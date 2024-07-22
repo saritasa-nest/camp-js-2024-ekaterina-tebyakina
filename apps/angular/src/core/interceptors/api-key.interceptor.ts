3aa9b903add89d729804e6ff2fe6737492198f5b
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-22 16:15:59 +0700

    Fix JSDoc comment JC-644

--- apps/angular/src/core/interceptors/api-key.interceptor.ts
+++ apps/angular/src/core/interceptors/api-key.interceptor.ts
@@ -3,7 +3,7 @@ import { AppConfig } from '../utils/app-config';
 
 const appConfig = new AppConfig;
 
-/** Adds header 'Api-Key' to a request. */
+/** Adds header Api-Key to a request. */
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
 	const reqWithApiKey = req.clone({
 		headers: req.headers.set('Api-Key', appConfig.apiKey),
