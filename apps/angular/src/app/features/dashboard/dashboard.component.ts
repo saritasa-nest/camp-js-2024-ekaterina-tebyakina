f93818c06ac85c911e6b49f0477b9b6717e774f6
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-07-22 17:37:34 +0700

    Add trackBy function to table JC-644

--- apps/angular/src/app/features/dashboard/dashboard.component.ts
+++ apps/angular/src/app/features/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, TrackByFunction } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { ApiService } from '@js-camp/angular/core/services/api.service';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { EmptyPipe } from '@js-camp/angular/shared/pipes/empty.pipe';
 import { ProgressBarComponent } from '@js-camp/angular/shared/components/progress-bar/progress-bar.component';
+import { Anime } from '@js-camp/core/models/anime';
 
 /** Column headers to be displayed in table. */
 export enum ColumnsHeaders {
 	Image = 'Image',
-	TitleEng = 'English title',
+	EnglishTitle = 'English title',
 	TitleJpn = 'Japanese title',
 	AiredStart = 'Aired starts with',
 	Type = 'type',
@@ -35,6 +36,16 @@ export class DashboardComponent {
 
 	/** Stream of anime. */
 	protected readonly anime$ = this.apiService.getAnime();
+
+	/**
+	 * Track by function for anime list.
+	 * @param index - Anime list item id.
+	 * @param item - Item of anime list.
+	 * @returns Item's id.
+	 */
+	protected trackByAnime(index: number, item: { id: number; }): number {
+		return item.id;
+	}
 
 	/** Getter for column headers enum to access enum from html. */
 	protected get columnsHeaders(): typeof ColumnsHeaders {
