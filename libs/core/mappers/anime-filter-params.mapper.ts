c6e3bc373d7f259f1775404cd5ae363d390bc843
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-02 13:13:07 +0700

    Rename and refactoring mapper for filter params JC-660

---
+++ libs/core/mappers/anime-filter-params.mapper.ts
@@ -0,0 +1,69 @@
+import { AnimeTypeDto } from '../dtos/anime-type.dto';
+import { AnimeFilterParamsDto } from '../dtos/anime-filter-params.dto';
+import {
+	AnimeFilterParams,
+	DEFAULT_PAGE_INDEX,
+	DEFAULT_PAGE_SIZE,
+	DEFAULT_SEARCH_TERM,
+	DEFAULT_SORT_SETTINGS,
+	DEFAULT_TYPE,
+} from '../models/anime-filter-params';
+import { checkIsEnumMember } from '../utils/chek-is-enum.util';
+
+import { AnimeSortMapper } from './anime-sort.mapper';
+import { AnimeTypeMapper } from './anime-type.mapper';
+
+export namespace AnimeFilterParamsMapper {
+
+	const DEFAULT_OFFSET = 0;
+	const DEFAULT_LIMIT = 25;
+	const DEFAULT_SEARCH = '';
+
+	/**
+	 * Map anime filter params dto to model.
+	 * @param params - Anime filter params dto.
+	 * @returns Model with anime filter params for UI.
+	 */
+	export function fromDto(params: AnimeFilterParamsDto): AnimeFilterParams {
+
+		const pageIndex = (params.offset && params.limit) ? (params.offset / params.limit) : DEFAULT_PAGE_INDEX;
+
+		let selectedTypes = DEFAULT_TYPE;
+		if (params.type__in) {
+			const typesArray = params.type__in.split(',');
+			const filteredArray = typesArray.filter(item => checkIsEnumMember(item, AnimeTypeDto));
+
+			// Using ‘as’ to avoid typescript error, but the type is checked by the checkIsEnumMember function previously.
+			selectedTypes = filteredArray.map(type => AnimeTypeMapper.fromDto(type as AnimeTypeDto));
+		}
+
+		const filterParams: AnimeFilterParams = {
+			pageIndex,
+			pageSize: params.limit ? params.limit : DEFAULT_PAGE_SIZE,
+			searchTerm: params.search ? params.search : DEFAULT_SEARCH_TERM,
+			selectedTypes,
+			sortingSettings: params.ordering ? AnimeSortMapper.fromDto(params.ordering) : DEFAULT_SORT_SETTINGS,
+		};
+
+		return filterParams;
+	}
+
+	/**
+	 * Map  anime filter params model to dto.
+	 * @param params - Model with anime filter params for UI.
+	 * @returns Anime filter params dto.
+	 */
+	export function toDto(params: Partial<AnimeFilterParams>): AnimeFilterParamsDto {
+
+		return {
+			offset: params.pageSize && params.pageIndex ? params.pageSize * params.pageIndex : DEFAULT_OFFSET,
+			limit: params.pageSize ? params.pageSize : DEFAULT_LIMIT,
+			search: params.searchTerm ? params.searchTerm : DEFAULT_SEARCH,
+			ordering: params.sortingSettings ? AnimeSortMapper.toDto(params.sortingSettings) : '',
+
+			// Disable eslint, because this property should be in snake case.
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			type__in: params.selectedTypes ? params.selectedTypes.map(animeType => AnimeTypeMapper.toDto(animeType)).join(',') : '',
+		};
+	}
+}
