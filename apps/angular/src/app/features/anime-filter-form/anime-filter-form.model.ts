e40af066195cbf301b01b501eda2fc7b3ac83b03
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-02 13:08:30 +0700

    Create model for form JC-660

---
+++ apps/angular/src/app/features/anime-filter-form/anime-filter-form.model.ts
@@ -0,0 +1,46 @@
+import { FormControl, NonNullableFormBuilder, FormGroup } from '@angular/forms';
+import { AnimeType } from '@js-camp/core/models/anime-type';
+
+/** Type for anime filter form group. Contains types for each control. */
+export type AnimeFilterForm = {
+
+	/** Type of form control to select the type of anime. */
+	readonly types: FormControl<AnimeType[]>;
+
+	/** Type of form control to search anime. */
+	readonly search: FormControl<string>;
+};
+
+/** Type of parameters of the form initialization function. */
+export type AnimeFilterFormParams = {
+
+	/** From builder for form. */
+	formBuilder: NonNullableFormBuilder;
+
+	/** Initial value for types form control. */
+	typesInitialValue: AnimeType[];
+
+	/** Initial value for search form control. */
+	searchInitialValue: string;
+};
+
+export namespace AnimeFilterForm {
+
+	/**
+	 * Function for initializing anime form filter.
+	 * @param typesInitialValue - Initial value for types form control.
+	 * @param searchInitialValue - Initial value for search form control.
+	 * @param formBuilder - From builder for form.
+	 * @returns Anime form.
+	 */
+	export function initialize({
+		formBuilder,
+		searchInitialValue,
+		typesInitialValue,
+	}: AnimeFilterFormParams): FormGroup<AnimeFilterForm> {
+		return formBuilder.group({
+			types: formBuilder.control(typesInitialValue),
+			search: formBuilder.control(searchInitialValue),
+		});
+	}
+}
