d67c98358e9c6b568ce7de68805950739a274bba
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-02 13:16:14 +0700

    Rename enums JC-660

--- libs/core/models/anime-columns-indexes.ts
+++ libs/core/models/anime-columns-indexes.ts
@@ -1,5 +1,5 @@
 /** Column indexes to be table columns. */
-export enum ColumnsIndexes {
+export enum AnimeColumnsIndexes {
 	Image = 'image',
 	EnglishTitle = 'title_eng',
 	JapaneseTitle = 'title_jpn',
