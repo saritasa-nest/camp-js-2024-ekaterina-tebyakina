ba35f5d74cf603a40b99f0263fd6d391fb45d284
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-02 13:12:08 +0700

    Fix comment JC-660

--- libs/core/dtos/anime-sort.dto.ts
+++ libs/core/dtos/anime-sort.dto.ts
@@ -1,2 +1,2 @@
-/** Type for sorting settings. Used in query params and in params for request to the server. */
+/** Type for sorting settings. Used in params for request to the server. */
 export type AnimeSortDto = string;
