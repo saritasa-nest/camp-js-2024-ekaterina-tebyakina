34efc86ee2ac920d45e08fdd700e9090cd053aa8
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-02 13:17:41 +0700

    Refactoring anime sort model JC-660

--- libs/core/models/anime-sort.ts
+++ libs/core/models/anime-sort.ts
@@ -1,14 +1,12 @@
-import { ColumnsIndexes } from './columns-indexes';
-
 /**
  * Sort settings for UI.
  * They are responsible for how the components should look according to the selected sorting settings.
  */
 export type AnimeSort = {
 
-	/** */
-	active: ColumnsIndexes | '';
+	/** Selected field for sort. */
+	readonly sortField: string;
 
-	/** */
-	direction: '' | 'asc' | 'desc';
+	/** Selected direction of sort. */
+	readonly direction: '' | 'asc' | 'desc';
 };
