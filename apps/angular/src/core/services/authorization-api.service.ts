7e9371f0236c414e6adc9c298b4767c0ecd56722
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-05 12:06:53 +0700

    Development authorization JC-679

--- apps/angular/src/core/services/authorization-api.service.ts
+++ apps/angular/src/core/services/authorization-api.service.ts
@@ -1,11 +1,10 @@
-// import { Observable, map, tap } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Registration } from '@js-camp/core/models/registration';
 import { RegistrationMapper } from '@js-camp/core/mappers/registration.mapper';
 import { AuthorizationTokens } from '@js-camp/core/models/authorization-tokens';
 import { Login } from '@js-camp/core/models/login';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 /** Authorization API access service. */
 @Injectable({ providedIn: 'root' })
@@ -18,12 +17,10 @@ export class AuthorizationApiService {
 	 * @param registrationData - Params for request.
 	 * @returns Page of anime list.
 	 */
-	public register(registrationData: Registration): void {
-		this.http.post<AuthorizationTokens>('auth/register/', RegistrationMapper.toDto(registrationData))
-			.subscribe(response => {
-				console.log('registrationData:', response);
-				this.saveTokens(response);
-			});
+	public register(registrationData: Registration): Observable<AuthorizationTokens> {
+		return this.http.post<AuthorizationTokens>('auth/register/', RegistrationMapper.toDto(registrationData)).pipe(
+			catchError(error => this.handleError(error)),
+		);
 	}
 
 	/**
@@ -32,21 +29,9 @@ export class AuthorizationApiService {
 	 * @returns Page of anime list.
 	 */
 	public login(loginData: Login): Observable<AuthorizationTokens> {
-		// this.http.post<AuthorizationTokens>('auth/login/', loginData)
-		// 	.subscribe(response => {
-		// 		console.log('authData:', response);
-		// 		this.saveTokens(response);
-		// 	});
-		return this.http.post<AuthorizationTokens>('auth/login/', loginData);
-		// .subscribe({
-		//     next: (response) => {
-		//         console.log('authData:', response);
-		//         this.saveTokens(response);
-		//     },
-		//     error: (err) => {
-		//         console.error('AuthData failed:', err);
-		//     }
-		// });
+		return this.http.post<AuthorizationTokens>('auth/login/', loginData).pipe(
+			catchError(error => this.handleError(error)),
+		);
 	}
 
 	/**
@@ -59,18 +44,32 @@ export class AuthorizationApiService {
 		localStorage.setItem('refreshToken', tokens.refresh);
 	}
 
-	private handleError(error: HttpErrorResponse): void {
-		// You can display a general error message to the user
-		if (error.error instanceof ErrorEvent) {
-			console.error('An error occurred:', error.error.message);
-		} else {
-			console.error(`Backend returned code ${error.status}, body was: `, error.error);
-		}
+	public refreshAccessToken(): Observable<any> {
+    const refreshToken = localStorage.getItem('refreshToken');
+    return this.http.post<AuthorizationTokens>('auth/token/refresh', { refreshToken }).pipe(
+      tap((response) => {
+        localStorage.setItem('accessToken', response.access);
+      }),
+      catchError(error => this.handleError(error))
+    );
+  }
 
-		// Assuming you want to set an error message in the form
-		const serverErrorMessage = error.error.message || 'Authorization failed. Please try again.';
+  public logout(): void {
+    // Your logout logic here
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+  }
 
-		// Set the error in a form control (you might want to create a separate control for errors)
-		// this.registrationForm.setErrors({ serverError: serverErrorMessage });
+	/**
+	 * Get page of anime list.
+	 * @returns Page of anime list.
+	 */
+	public getAuthorizationToken(): string | null {
+		const accessToken = localStorage.getItem('accessToken');
+		return accessToken;
+	}
+
+	private handleError(error: HttpErrorResponse): Observable<never> {
+		return throwError(() => error);
 	}
 }
