38e10e06ec53a682c49ddf242ae604d644213e46
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-13 14:28:31 +0700

    Fix merge issues  JC-679

--- libs/core/dtos/sorting-columns.dto.ts
+++
@@ -1,6 +0,0 @@
-/** Column headers to be displayed in table. */
-export enum SortingColumnsDto {
-	EnglishTitle = 'title_eng',
-	AiredStart = 'aired__startswith',
-	Status = 'status',
-}
--- libs/core/models/sorting-columns.ts
+++
@@ -1,6 +0,0 @@
-/** Column headers to be displayed in table. */
-export enum SortingColumns {
-	EnglishTitle = 'English title',
-	AiredStart = 'Aired starts with',
-	Status = 'Status',
-}
