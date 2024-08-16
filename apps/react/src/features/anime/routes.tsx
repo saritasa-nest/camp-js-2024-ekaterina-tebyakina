565c2c6546065c12c76328a42b9efc1536af5e34
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-16 18:26:02 +0700

    Setup routes for anime page JC-720

--- apps/react/src/features/anime/routes.tsx
+++ apps/react/src/features/anime/routes.tsx
@@ -1,15 +1,34 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 
+import { AnimeCard } from './components/AnimeCard';
+import { AnimeEditForm } from './components/AnimeEditForm';
+
 const AnimePage = lazy(() => import('./pages/AnimePage').then(module => ({ default: module.AnimePage })));
 
 /** Path to anime for url. */
 export const PATH_TO_ANIME = 'anime';
+
+/** Path to anime details for url. */
+export const PATH_TO_ANIME_DETAILS = ':id';
+
+/** Path to a anime editing for url. */
+export const PATH_TO_EDIT_ANIME = 'edit';
 
 /** Route object for AnimePage. */
 export const animeRoutes: RouteObject[] = [
 	{
 		path: PATH_TO_ANIME,
 		element: <AnimePage />,
+		children: [
+			{
+				path: PATH_TO_ANIME_DETAILS,
+				element: <AnimeCard />,
+			},
+			{
+				path: `${PATH_TO_ANIME_DETAILS}/${PATH_TO_EDIT_ANIME}`,
+				element: <AnimeEditForm />,
+			},
+		],
 	},
 ];
