450c49035fb1be14c417c99164d8a8e77140a58a
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-16 18:25:45 +0700

    Add anime page JC-720

--- apps/react/src/features/anime/pages/AnimePage.tsx
+++ apps/react/src/features/anime/pages/AnimePage.tsx
@@ -1,11 +1,40 @@
-import { memo, FC } from 'react';
+import { memo, FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
+import { selectAnimeList, selectAnimeListLoading } from '@js-camp/react/store/anime/selectors';
+import { fetchList } from '@js-camp/react/store/anime/dispatchers';
+import { Outlet } from 'react-router-dom';
+
+import { AnimeList } from '../components/AnimeList';
+import { AnimeFilterForm } from '../components/AnimeFilterForm';
+
+import styles from './AnimePage.module.css';
 
 /** Anime page component. */
-const AnimePageComponent: FC = () => (
-	<>
-		<h1>Anime</h1>
-	</>
-);
+const AnimePageComponent: FC = () => {
+	const dispatch = useAppDispatch();
+	const animeList = useAppSelector(selectAnimeList);
+	const isLoading = useAppSelector(selectAnimeListLoading);
+
+	useEffect(() => {
+		dispatch(fetchList());
+	}, [dispatch]);
+
+	if (isLoading) {
+		return <div>Loading</div>;
+	}
+
+	return (
+		<main className={styles.main}>
+			<div className={styles.main__section}>
+				<AnimeFilterForm />
+				<AnimeList animeList={animeList} />
+			</div>
+			<div className={styles.main__details}>
+				<Outlet />
+			</div>
+		</main>
+	);
+};
 
 /** Memorized anime page component. */
 export const AnimePage = memo(AnimePageComponent);
