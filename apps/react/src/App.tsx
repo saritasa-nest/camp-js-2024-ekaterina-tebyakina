f0541ab873c3cee5128f2dcba73fd1514e685194
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-19 10:58:50 +0700

    Add StyledEngineProvider to avoid important keyword in css JC-702

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -2,6 +2,7 @@
 import { FC, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material';
 
 import { RootRouter } from './routes/RootRouter';
 import { store } from './store';
@@ -14,10 +15,13 @@ export const App: FC = () => (
 	<Provider store={store}>
 		<BrowserRouter>
 			<div>
-				<Suspense fallback={<div>Brrr... here should be your loader component</div>}>
-					<Header />
-					<RootRouter />
-				</Suspense>
+				<StyledEngineProvider injectFirst>
+					<Suspense fallback={<div>Brrr... here should be your loader component</div>}>
+						<Header />
+						<RootRouter />
+					</Suspense>
+				</StyledEngineProvider>
+
 			</div>
 		</BrowserRouter>
 	</Provider>
