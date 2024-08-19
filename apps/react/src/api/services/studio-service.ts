b08a0c01807cf71978c9fbde634ab51c5065872a
manpham-saritasa <man.pham@saritasa.com>
2024-08-19 11:22:51 +0700

    Initialize studio service JC-718

---
+++ apps/react/src/api/services/studio-service.ts
@@ -0,0 +1,21 @@
+import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
+
+import { AnimeStudioMapper } from '@js-camp/core/mappers/anime-studio.mapper';
+import { AnimeStudioDto } from '@js-camp/core/dtos/anime-studio.dto';
+
+import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
+
+import { http } from '..';
+import { AppUrls } from '../app-url';
+
+export namespace StudioService {
+
+	/** Get all studios. */
+	export async function getAllStudios() {
+		const studioPage = await http
+			.get<PaginationDto<AnimeStudioDto>>(AppUrls.anime.studio.listCursor)
+			.then(responseDto => PaginationMapper.fromDto(responseDto.data, AnimeStudioMapper.fromDto));
+
+		return studioPage;
+	}
+}
