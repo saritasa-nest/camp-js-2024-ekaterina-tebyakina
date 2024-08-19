e4537196c93319828c9c766de3c5feffd6e840f4
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-19 19:57:20 +0700

    Add anime type mappers JC-720

--- libs/core/mappers/anime-type.mapper.ts
+++ libs/core/mappers/anime-type.mapper.ts
@@ -1,5 +1,6 @@
 import { AnimeTypeDto } from '../dtos/anime-type.dto';
 import { AnimeType } from '../models/anime-type';
+import { checkIsEnumMember } from '../utils/check-is-enum.util';
 
 export namespace AnimeTypeMapper {
 	const ANIME_TYPE_MAP_FROM_DTO: Readonly<Record<AnimeTypeDto, AnimeType>> = {
@@ -40,5 +41,47 @@ export namespace AnimeTypeMapper {
 	 */
 	export function toDto(type: AnimeType): AnimeTypeDto {
 		return ANIME_TYPE_MAP_TO_DTO[type];
+	}
+
+	/**
+	 * Array to string.
+	 * @param typesArray  - Types array.
+	 * @returns String with types.
+	 */
+	export function arrayToString(typesArray: AnimeType[]): string {
+		return typesArray.join(',');
+	}
+
+	/**
+	 * String to array.
+	 * @param typesString - String with types.
+	 * @returns Types array.
+	 */
+	export function stringToArray(typesString: string): AnimeType[] {
+		const typesArray = typesString.split(',');
+		assertIsAnimeTypeArray(typesArray);
+		return typesArray;
+	}
+
+	/**
+	 * Assert that value is typeof AnimeType[].
+	 * @param array - Value for checking.
+	 */
+	function assertIsAnimeTypeArray(array: unknown[]): asserts array is AnimeType[] {
+		array.forEach(item => {
+			if (!checkIsEnumMember(item, AnimeType)) {
+				throw new Error(`${item} is not a valid AnimeType!`);
+			}
+		});
+	}
+
+	/**
+	 * Assert that value is typeof AnimeType.
+	 * @param value - Value for checking.
+	 */
+	export function assertIsAnimeType(value: unknown): asserts value is AnimeType {
+		if (!checkIsEnumMember(value, AnimeType)) {
+			throw new Error(`${value} is not a valid AnimeType!`);
+		}
 	}
 }
