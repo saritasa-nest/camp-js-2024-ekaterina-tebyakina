7341a02251412094246adf3bddf5ef2a0aa3995a
manpham-saritasa <man.pham@saritasa.com>
2024-08-20 16:24:08 +0700

    Initialize get all studios with query params JC-718

--- apps/react/src/api/services/studio-service.ts
+++ apps/react/src/api/services/studio-service.ts
@@ -5,14 +5,31 @@ import { AnimeStudioDto } from '@js-camp/core/dtos/anime-studio.dto';
 
 import { PaginationListCursorMapper } from '@js-camp/core/mappers/pagination.mapper';
 
+import { StudioQueryParams, StudioQueryParamsMapper } from '@js-camp/core/mappers/studio-query-params.mapper';
+
+import { StudioFilterParamsMapper } from '@js-camp/core/mappers/studio-filter-params.mapper';
+
 import { http } from '..';
 import { AppUrls } from '../app-url';
 
 export namespace StudioService {
 
-	/** Get all studios. */
+	/**
+	 * Get all studios.
+	 * @param queryParams Query params.
+	 */
 	export async function getAllStudios() {
 		const { data } = await http.get<PaginationListCursorDto<AnimeStudioDto>>(AppUrls.anime.studio.listCursor);
+
+		return PaginationListCursorMapper.fromDto(data, AnimeStudioMapper.fromDto);
+	}
+
+	export async function getAllStudios2(queryParams: StudioQueryParams) {
+		const filterParams = StudioQueryParamsMapper.fromDto(queryParams);
+		const filterParamsDto = StudioFilterParamsMapper.toDto(filterParams);
+		const { data } = await http.get<PaginationListCursorDto<AnimeStudioDto>>(AppUrls.anime.studio.listCursor, {
+			params: filterParamsDto,
+		});
 
 		return PaginationListCursorMapper.fromDto(data, AnimeStudioMapper.fromDto);
 	}
