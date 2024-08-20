a330daaa14cb9a7b58634bbb27085bb2e766f566
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-20 19:22:54 +0700

    Remove default route JC-720

--- apps/react/src/routes/RootRouter.tsx
+++ apps/react/src/routes/RootRouter.tsx
@@ -1,15 +1,11 @@
 import { FC } from 'react';
-import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
+import { RouteObject, useRoutes } from 'react-router-dom';
 
 import { genresRoutes } from '../features/genres/routes';
-import { animeRoutes, ANIME_PATH } from '../features/anime/routes';
+import { animeRoutes } from '../features/anime/routes';
 import { studiosRoutes } from '../features/studios/routes';
 
 const routes: RouteObject[] = [
-	{
-		path: '*',
-		element: <Navigate to={ANIME_PATH} />,
-	},
 	...animeRoutes,
 	...genresRoutes,
 	...studiosRoutes,
