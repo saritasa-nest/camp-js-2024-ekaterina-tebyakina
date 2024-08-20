61d59501d6f2ecc62c7c4d346b8a2a35473f4555
manpham-saritasa <man.pham@saritasa.com>
2024-08-20 17:50:41 +0700

    Handle previous state when action is fulfilled JC-718

--- apps/react/src/store/studio/slice.ts
+++ apps/react/src/store/studio/slice.ts
@@ -8,19 +8,24 @@ export const studiosSlice = createSlice({
 	name: 'studio',
 	initialState,
 	reducers: {},
-	extraReducers: builder => builder
-		.addCase(getAllStudios.pending, state => {
-			state.isLoading = true;
-		})
-		.addCase(getAllStudios.fulfilled, (state, action) => {
-			state.studios = [...action.payload.results];
-			state.isLoading = false;
-			state.next = action.payload.next;
-		})
-		.addCase(getAllStudios.rejected, (state, action) => {
-			if (action.error.message) {
-				state.error = action.error.message;
-			}
-			state.isLoading = false;
-		}),
+	extraReducers: builder =>
+		builder
+			.addCase(getAllStudios.pending, state => {
+				state.isLoading = true;
+			})
+			.addCase(getAllStudios.fulfilled, (state, action) => {
+				if (action.payload.previous != null) {
+					state.studios = [...state.studios, ...action.payload.results];
+				} else {
+					state.studios = [...action.payload.results];
+				}
+				state.next = action.payload.next;
+				state.isLoading = false;
+			})
+			.addCase(getAllStudios.rejected, (state, action) => {
+				if (action.error.message) {
+					state.error = action.error.message;
+				}
+				state.isLoading = false;
+			}),
 });
