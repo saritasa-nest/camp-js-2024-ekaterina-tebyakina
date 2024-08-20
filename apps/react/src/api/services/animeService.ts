e2bae26506d49cad282fe9f8705324facd37f5b2
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-20 17:57:41 +0700

    Update anime service JC-720

--- apps/react/src/api/services/animeService.ts
+++ apps/react/src/api/services/animeService.ts
@@ -2,24 +2,35 @@ import { Anime } from '@js-camp/core/models/anime';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
 import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
-import { AnimeParams } from '@js-camp/react/models/animeParams';
+import { AnimeFilterParams } from '@js-camp/react/models/animeFilterParams';
+import { Pagination } from '@js-camp/core/models/pagination';
+import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 
 import { http } from '..';
-import { AnimeParamsMapper } from '../mappers/animeParamsMapper';
+import { AnimeFilterParamsMapper } from '../mappers/animeFilterParamsMapper';
 
-const url = 'anime/anime/';
+const animeListUrl = 'anime/anime/list-cursor/';
 
 export namespace AnimeService {
 
 	/**
 	 * Fetches a list of anime.
-	 * @param params - AnimeParams.
+	 * @param params - Params for filtering anime.
 	 */
-	export async function fetchList(params: AnimeParams): Promise<Anime[]> {
-		console.log(AnimeParamsMapper.toDto(params));
-		const { data } = await http.get<PaginationDto<AnimeDto>>(url, {
-			params: AnimeParamsMapper.toDto(params),
+	export async function fetchList(params: AnimeFilterParams): Promise<Pagination<Anime>> {
+		const { data } = await http.get<PaginationDto<AnimeDto>>(animeListUrl, {
+			params: AnimeFilterParamsMapper.toDto(params),
 		});
-		return data.results.map(dto => AnimeMapper.fromDto(dto));
+
+		return PaginationMapper.fromDto(data, AnimeMapper.fromDto);
+	}
+
+	/**
+	 * Fetches a new page of anime list.
+	 * @param newPageUrl - Url of new page.
+	 */
+	export async function fetchNewPage(newPageUrl: string): Promise<Pagination<Anime>> {
+		const { data } = await http.get<PaginationDto<AnimeDto>>(newPageUrl);
+		return PaginationMapper.fromDto(data, AnimeMapper.fromDto);
 	}
 }
