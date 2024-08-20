ae804d17795c804fd14e5e924a1e9bfe0a5279a4
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-20 18:06:17 +0700

    Update anime page by embedding logic in separate components JC-720

--- apps/react/src/features/anime/pages/AnimePage.tsx
+++ apps/react/src/features/anime/pages/AnimePage.tsx
@@ -1,46 +1,23 @@
-import { memo, FC, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
-import { selectAnimeList, selectAnimeListLoading } from '@js-camp/react/store/anime/selectors';
-import { fetchList } from '@js-camp/react/store/anime/dispatchers';
+import { memo, FC } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { AnimeSort } from '@js-camp/react/models/animeSort';
-
-import { AnimeList } from '../components/AnimeList';
-import { AnimeFilterForm } from '../components/AnimeFilterForm';
+import { AnimeFilterForm } from '../components/AnimeFilterForm/AnimeFilterForm';
+import { AnimeList } from '../components/AnimeList/AnimeList';
 
 import styles from './AnimePage.module.css';
 
 /** Anime page component. */
-const AnimePageComponent: FC = () => {
-	const dispatch = useAppDispatch();
-	const animeList = useAppSelector(selectAnimeList);
-	const isLoading = useAppSelector(selectAnimeListLoading);
+const AnimePageComponent: FC = () => (
+	<main className={styles.main}>
+		<div className={styles.section}>
+			<AnimeFilterForm />
+			<AnimeList />
+		</div>
+		<div className={styles.details}>
+			<Outlet />
+		</div>
+	</main>
+);
 
-	useEffect(() => {
-		dispatch(fetchList({
-			searchTerm: '',
-			selectedTypes: [],
-			sortingSettings: AnimeSort.None,
-		}));
-	}, [dispatch]);
-
-	if (isLoading) {
-		return <div>Loading</div>;
-	}
-
-	return (
-		<main className={styles.main}>
-			<div className={styles.main__section}>
-				<AnimeFilterForm />
-				<AnimeList animeList={animeList} />
-			</div>
-			<div className={styles.main__details}>
-				<Outlet />
-			</div>
-		</main>
-	);
-};
-
-/** Memorized anime page component. */
+/** Memoized anime page component. */
 export const AnimePage = memo(AnimePageComponent);
