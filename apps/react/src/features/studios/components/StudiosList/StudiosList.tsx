aa15fb152d085aa6aaf4bd5d1ba05c8bd8abd5b7
manpham-saritasa <man.pham@saritasa.com>
2024-08-20 11:26:56 +0700

    Migrate the fetching action to studio list and implement StudioListItem JC-718

--- apps/react/src/features/studios/components/StudiosList/StudiosList.tsx
+++ apps/react/src/features/studios/components/StudiosList/StudiosList.tsx
@@ -1,8 +1,13 @@
-import { memo, FC } from 'react';
+import { memo, FC, useEffect, useRef } from 'react';
 import { List, ListItem, IconButton, ListItemAvatar, ListItemText, Avatar } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import { AnimeStudio } from '@js-camp/core/models/anime-studio';
+import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
+import { selectAreStudiosLoading, selectStudios } from '@js-camp/react/store/studio/selectors';
+import { getAllStudios } from '@js-camp/react/store/studio/dispatchers';
+
+import { StudioListItem } from '../StudioListItem/StudioListItem';
 
 type Props = {
 
@@ -11,43 +16,64 @@ type Props = {
 };
 
 /** Studios list.  */
-const StudiosListComponent: FC<Props> = ({ studios }: Props) => (
-	<List
-		sx={{
-			height: '100%',
-			overflowY: 'auto',
-			borderWidth: '1px',
-			borderStyle: 'solid',
-			borderColor: 'rgba(0 0 0 / 15%)',
-		}}
-		disablePadding
-	>
-		{studios.map(studio => (
-			<ListItem
-				key={studio.id}
-				alignItems="center"
-				sx={{
-					'borderBottom': 1,
-					'borderColor': 'rgba(0 0 0 / 15%)',
-					':hover': {
-						cursor: 'pointer',
-						backgroundColor: 'rgba(0 0 0 / 15%)',
-					},
-				}}
-				secondaryAction={
-					<IconButton edge="end" aria-label="delete">
-						<DeleteIcon />
-					</IconButton>
+const StudiosListComponent: FC = () => {
+	const dispatch = useAppDispatch();
+	const studiosList = useAppSelector(selectStudios);
+	const isLoading = useAppSelector(selectAreStudiosLoading);
+
+	const containerRef = useRef(null);
+
+	const options: IntersectionObserverInit = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.5,
+	};
+	const callbackFn: IntersectionObserverCallback = entries => {
+		const [entry] = entries;
+		if (entry.isIntersecting) {
+			console.log('hey i am in the view');
+		}
+	};
+
+	useEffect(() => {
+		dispatch(getAllStudios());
+	}, [dispatch]);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(callbackFn, options);
+		if (containerRef.current) {
+			observer.observe(containerRef.current);
+		}
+		return () => {
+			if (containerRef.current) {
+				observer.unobserve(containerRef.current);
+			}
+		};
+	}, [containerRef, options]);
+
+	if (isLoading) {
+		return <div>Loading</div>;
+	}
+	return (
+		<List
+			sx={{
+				height: '100%',
+				overflowY: 'auto',
+				borderWidth: '1px',
+				borderStyle: 'solid',
+				borderColor: 'rgba(0 0 0 / 15%)',
+			}}
+			disablePadding
+		>
+			{studiosList.map((studio, index) => {
+				if (studiosList.length === index + 1) {
+					return <StudioListItem ref={containerRef} key={studio.id} studio={studio} />;
 				}
-			>
-				<ListItemAvatar sx={{ marginRight: '16px' }}>
-					<Avatar sx={{ width: 80, height: 80 }} alt={studio.name} src={studio.image} />
-				</ListItemAvatar>
-				<ListItemText primary={studio.name} />
-			</ListItem>
-		))}
-	</List>
-);
+				return <StudioListItem key={studio.id} studio={studio} />;
+			})}
+		</List>
+	);
+};
 
 /** Memorized studios list. */
 export const StudiosList = memo(StudiosListComponent);
--- apps/react/src/features/studios/pages/StudiosPage/StudioPage.tsx
+++ apps/react/src/features/studios/pages/StudiosPage/StudioPage.tsx
@@ -1,40 +1,21 @@
-import { memo, useEffect, FC } from 'react';
+import { memo, FC } from 'react';
 import { Outlet } from 'react-router-dom';
-
-import { selectAreStudiosLoading, selectStudios } from '@js-camp/react/store/studio/selectors';
-import { getAllStudios } from '@js-camp/react/store/studio/dispatchers';
-
-import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
 
 import { StudiosList } from '../../components/StudiosList';
 
 import styles from './StudioPage.module.css';
 
 /** Studio page component. */
-const StudiosPageComponent: FC = () => {
-	const dispatch = useAppDispatch();
-	const studios = useAppSelector(selectStudios);
-	const isLoading = useAppSelector(selectAreStudiosLoading);
-
-	useEffect(() => {
-		dispatch(getAllStudios());
-	}, [dispatch]);
-
-	if (isLoading) {
-		return <div>Loading</div>;
-	}
-
-	return (
-		<main className={styles.main}>
-			<div className={styles.main__section}>
-				<StudiosList studios={studios} />
-			</div>
-			<div className={styles.main__details}>
-				<Outlet />
-			</div>
-		</main>
-	);
-};
+const StudiosPageComponent: FC = () => (
+	<main className={styles.main}>
+		<div className={styles.main__section}>
+			<StudiosList />
+		</div>
+		<div className={styles.main__details}>
+			<Outlet />
+		</div>
+	</main>
+);
 
 /** Memorized studio page component. */
 export const StudiosPage = memo(StudiosPageComponent);
