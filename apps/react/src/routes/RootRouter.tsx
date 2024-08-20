2113f69af51af4de8e340f413ac26c7e3734e66d
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-20 19:25:08 +0700

    Add default route JC-720

--- apps/react/src/routes/RootRouter.tsx
+++ apps/react/src/routes/RootRouter.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
-import { RouteObject, useRoutes } from 'react-router-dom';
+import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
 
 import { genresRoutes } from '../features/genres/routes';
-import { animeRoutes } from '../features/anime/routes';
+import { animeRoutes, ANIME_PATH } from '../features/anime/routes';
 import { studiosRoutes } from '../features/studios/routes';
 
 const routes: RouteObject[] = [
+	{
+		path: '*',
+		element: <Navigate to={ANIME_PATH} />,
+	},
 	...animeRoutes,
 	...genresRoutes,
 	...studiosRoutes,
