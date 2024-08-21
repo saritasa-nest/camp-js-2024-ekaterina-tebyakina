0f11cdb44eecfb594a5e8dd02d22c9f40643d608
manpham-saritasa <man.pham@saritasa.com>
2024-08-21 15:51:46 +0700

    Initialize Studio search bar JC-718

---
+++ apps/react/src/features/studios/components/StudioSearchBar/StudioSearchBar.tsx
@@ -0,0 +1,62 @@
+import { FC, memo, useState } from 'react';
+import Box from '@mui/material/Box';
+import useQueryParams from '@js-camp/react/hooks/useQueryParam';
+import { Button, IconButton, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+/**
+ * Genres text field component.
+ * @param props Props.
+ */
+const StudioSearchBarComponent: FC = () => {
+	const { getQueryParamByKey, setQueryParams } = useQueryParams();
+	const searchParam = getQueryParamByKey('search');
+
+	const [value, setValue] = useState(searchParam);
+
+	/** Submit the search value to the URL. */
+	function handleSubmitSearchValue(): void {
+		const searchValue = value.length > 0 ? value : null;
+		setQueryParams({ search: searchValue });
+	}
+
+	/** Submit the search value to the URL. */
+	function handleResetSearchValue(): void {
+		setValue('');
+		if (searchParam.length > 0) {
+			setQueryParams({ search: null });
+		}
+	}
+
+	return (
+		<Box component="form" display="flex" padding={2} gap={1}>
+			<TextField
+				sx={{
+					flex: '1 1 0',
+				}}
+				label={'Search'}
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				InputProps={{
+					endAdornment: (
+						<IconButton
+							sx={{ display: value.length > 0 ? 'initial' : 'none', aspectRatio: 1 }}
+							onClick={handleResetSearchValue}
+							edge="end"
+							disabled={value.length === 0}
+						>
+							<CancelIcon />
+						</IconButton>
+					),
+				}}
+			/>
+			<Button type="button" variant="contained" onClick={handleSubmitSearchValue}>
+				<SearchIcon />
+			</Button>
+		</Box>
+	);
+};
+
+/** Memoized GenresTextFieldComponent. */
+export const StudioSearchBar = memo(StudioSearchBarComponent);
---
+++ apps/react/src/features/studios/components/StudioSearchBar/index.ts
@@ -0,0 +1,1 @@
+export { StudioSearchBar } from './StudioSearchBar';
