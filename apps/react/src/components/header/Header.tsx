009e660380bbbf38c47244ab605426a1d2a6eb3d
TebyakinaEkaterina <ekaterina.tebyakina@saritasa.com>
2024-08-22 17:51:30 +0700

    Add authorization menu JC-726

--- apps/react/src/components/header/Header.tsx
+++ apps/react/src/components/header/Header.tsx
@@ -1,20 +1,41 @@
-import { FC, memo } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { FC, memo, useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import { ANIME_PATH } from '@js-camp/react/features/anime/routes';
 import { GENRES_PATH } from '@js-camp/react/features/genres/routes';
 import { STUDIOS_PATH } from '@js-camp/react/features/studios/routes';
+import { useAppDispatch, useAppSelector } from '@js-camp/react/store/store';
+import { selectCurrentUser, selectIsCurrentUserLoading } from '@js-camp/react/store/user/selectors';
+import { REGISTER_PATH } from '@js-camp/react/features/registration/routes';
+import { fetchUser } from '@js-camp/react/store/user/dispatchers';
+import { LOGIN_PATH } from '@js-camp/react/features/login/routes';
+import { logout } from '@js-camp/react/store/user/slice';
 
 import styles from './Header.module.css';
 
 /** Header component. */
 const HeaderComponent: FC = () => {
 
+	const user = useAppSelector(selectCurrentUser);
+	const isLoading = useAppSelector(selectIsCurrentUserLoading);
+
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		if (!isLoading) {
+			dispatch(fetchUser());
+		}
+	}, []);
+
 	const location = useLocation();
 
 	const isCurrentPath = (path: string) => location.pathname.startsWith(path);
+
+	const handleLogoutButtonClick = () => {
+		dispatch(logout());
+	};
 
 	return (
 		<AppBar
@@ -47,7 +68,24 @@ const HeaderComponent: FC = () => {
 					/>
 				</Stack>
 			</nav>
-			<span>Authorization menu</span>
+			<span className={styles.authorizationMenu}>
+				{ user ?
+					<>
+						<span>{`Hi, ${user.firstName} ${user.lastName}!`}</span>
+						<button
+							type='button'
+							className={styles.logoutButton}
+							onClick={handleLogoutButtonClick}
+						>
+							Logout
+						</button>
+					</>	:
+					<>
+						<NavLink to={LOGIN_PATH} className={styles.link}>Login</NavLink>
+						<NavLink to={REGISTER_PATH} className={styles.link}>Register</NavLink>
+					</>
+				}
+			</span>
 		</AppBar>
 	);
 };
