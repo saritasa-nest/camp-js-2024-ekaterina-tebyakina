563dc17f09d7741c13ecb34cba08089bbdccf8af
manpham-saritasa <man.pham@saritasa.com>
2024-08-23 14:23:44 +0700

    Add studio detail path and handle navigate to the detail JC-718

--- apps/react/src/features/studios/components/StudioListItem/StudioListItem.tsx
+++ apps/react/src/features/studios/components/StudioListItem/StudioListItem.tsx
@@ -4,6 +4,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { ListItem, ListItemText, IconButton, ListItemAvatar, Avatar } from '@mui/material';
 
 import { AnimeStudio } from '@js-camp/core/models/anime-studio';
+import { useNavigate } from 'react-router-dom';
+
+import { STUDIOS_PATH } from '../../routes';
 
 type Props = {
 
@@ -11,45 +14,55 @@ type Props = {
 	readonly studio: AnimeStudio;
 };
 
-const StudioListItemComponent = forwardRef<HTMLLIElement, Props>(({ studio }: Props, ref) => (
-	<ListItem
-		ref={ref}
-		alignItems='center'
-		sx={{
-			'borderBottom': 1,
-			'borderColor': 'rgba(0 0 0 / 15%)',
-			':hover': {
-				cursor: 'pointer',
-				backgroundColor: 'rgba(0 0 0 / 15%)',
-			},
-		}}
-		secondaryAction={
-			<IconButton
-				edge='end'
-				aria-label='delete'
-			>
-				<DeleteIcon />
-			</IconButton>
-		}
-	>
-		<ListItemAvatar
+const StudioListItemComponent = forwardRef<HTMLLIElement, Props>(({ studio }: Props, ref) => {
+	const navigate = useNavigate();
+
+	/** Navigate to studio detail. */
+	function handleNavigate() {
+		navigate(`/${STUDIOS_PATH}/${studio.id}`);
+	}
+
+	return (
+		<ListItem
+			ref={ref}
+			alignItems='center'
+			onClick={handleNavigate}
 			sx={{
-				marginRight: '16px',
+				'borderBottom': 1,
+				'borderColor': 'rgba(0 0 0 / 15%)',
+				':hover': {
+					cursor: 'pointer',
+					backgroundColor: 'rgba(0 0 0 / 15%)',
+				},
 			}}
+			secondaryAction={
+				<IconButton
+					edge='end'
+					aria-label='delete'
+				>
+					<DeleteIcon />
+				</IconButton>
+			}
 		>
-			<Avatar
+			<ListItemAvatar
 				sx={{
-					width: 80,
-					height: 80,
-					border: '1px solid rgba(0 0 0 / 15%)',
+					marginRight: '16px',
 				}}
-				alt={studio.name}
-				src={studio.image}
-			/>
-		</ListItemAvatar>
-		<ListItemText primary={studio.name} />
-	</ListItem>
-));
+			>
+				<Avatar
+					sx={{
+						width: 80,
+						height: 80,
+						border: '1px solid rgba(0 0 0 / 15%)',
+					}}
+					alt={studio.name}
+					src={studio.image}
+				/>
+			</ListItemAvatar>
+			<ListItemText primary={studio.name} />
+		</ListItem>
+	);
+});
 
 /** Memoized StudioListItemComponent. */
 export const StudioListItem = memo(StudioListItemComponent);
--- apps/react/src/features/studios/routes.tsx
+++ apps/react/src/features/studios/routes.tsx
@@ -1,15 +1,26 @@
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 
+import { StudioCard } from './components/StudioDetail';
+
 const StudiosPage = lazy(() => import('./pages/StudiosPage').then(module => ({ default: module.StudiosPage })));
 
 /** Path to studios for url. */
 export const STUDIOS_PATH = 'studios';
+
+/** Path to anime details for url. */
+export const STUDIO_DETAILS_PATH = ':id';
 
 /** Route object for studios page. */
 export const studiosRoutes: readonly RouteObject[] = [
 	{
 		path: STUDIOS_PATH,
 		element: <StudiosPage />,
+		children: [
+			{
+				path: STUDIO_DETAILS_PATH,
+				element: <StudioCard />,
+			},
+		],
 	},
 ];
