51c417f99b78e8716fb4f9700a41b5700a10bcaa
manpham-saritasa <man.pham@saritasa.com>
2024-08-23 10:46:21 +0700

    Implement ListItemSkeleton and loading spinner JC-718

--- apps/react/src/features/studios/components/StudiosList/StudiosList.tsx
+++ apps/react/src/features/studios/components/StudiosList/StudiosList.tsx
@@ -1,13 +1,15 @@
 import { memo, FC, useEffect, useRef, useCallback } from 'react';
-import { Box, List } from '@mui/material';
+import { Box, CircularProgress, List } from '@mui/material';
 
 import { useAppDispatch, useAppSelector } from '@js-camp/react/store';
-import { selectStudioNextCursor, selectStudios } from '@js-camp/react/store/studio/selectors';
+import { selectAreStudiosLoading, selectStudioNextCursor, selectStudios } from '@js-camp/react/store/studio/selectors';
 import { getAllStudios } from '@js-camp/react/store/studio/dispatchers';
 
 import useQueryParams from '@js-camp/react/hooks/useQueryParam';
 
 import { StudioQueryParams } from '@js-camp/core/mappers/studio-query-params.mapper';
+
+import { ListItemSkeleton } from '@js-camp/react/components/skeleton';
 
 import { StudioListItem } from '../StudioListItem';
 
@@ -22,6 +24,8 @@ const StudiosListComponent: FC = () => {
 	const sort = getQueryParamByKey('sort');
 
 	const wrapperElementRef = useRef<HTMLElement>(null);
+
+	const isLoading = useAppSelector(selectAreStudiosLoading);
 
 	useEffect(() => {
 		wrapperElementRef.current?.scrollTo({ top: 0, behavior: 'instant' });
@@ -56,7 +60,7 @@ const StudiosListComponent: FC = () => {
 
 	return (
 		<Box
-			component="section"
+			component='section'
 			ref={wrapperElementRef}
 			sx={{
 				flex: '1 1 0',
@@ -66,12 +70,35 @@ const StudiosListComponent: FC = () => {
 			}}
 		>
 			<List disablePadding>
+				{isLoading && studiosList.length === 0 && Array.from(new Array(10)).map(() => <ListItemSkeleton />)}
+
 				{studiosList.map((studio, index) => {
 					if (studiosList.length === index + 1) {
-						return <StudioListItem ref={lastElementRef} key={studio.id} studio={studio} />;
+						return (
+							<StudioListItem
+								ref={lastElementRef}
+								key={studio.id}
+								studio={studio}
+							/>
+						);
 					}
-					return <StudioListItem key={studio.id} studio={studio} />;
+					return (
+						<StudioListItem
+							key={studio.id}
+							studio={studio}
+						/>
+					);
 				})}
+
+				{isLoading && studiosList.length > 0 && (
+					<Box
+						display='flex'
+						justifyContent='center'
+						marginBlock={2}
+					>
+						<CircularProgress />
+					</Box>
+				)}
 			</List>
 		</Box>
 	);
