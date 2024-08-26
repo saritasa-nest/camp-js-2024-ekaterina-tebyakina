5c20304faeea6ee5710de378d930ca46038aa921
TebyakinaEkaterina <TebyakinaEkaterina@users.noreply.github.com>
2024-08-26 05:12:05 +0000

    Deploy preview for PR 11 🛫

--- pr-preview/pr-11/assets/AnimePage-DtNPDTXX.js
+++
@@ -1,1 +0,0 @@
-import{r as n,l as e}from"./index-Bcua0HtA.js";const m=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Anime"})}),r=n.memo(m);export{r as AnimePage};
---
+++ pr-preview/pr-11/assets/StudiosPage-BbZx_00V.js
@@ -0,0 +1,1 @@
+import{r as t,l as o}from"./index-Bpjo0uDh.js";const s=()=>o.jsx(o.Fragment,{children:o.jsx("h1",{children:"Studios"})}),r=t.memo(s);export{r as StudiosPage};
