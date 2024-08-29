97d213feeacf1a3ff72b723ad0c7c965b3f6436e
manpham-saritasa <man.pham@saritasa.com>
2024-08-29 14:26:03 +0700

    Apply the config

--- apps/react/src/theme/mui/index.ts
+++ apps/react/src/theme/mui/index.ts
@@ -1,30 +1,30 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-
-import { grey } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
 	palette: {
-		primary: {
-			main: 'rgb(63 81 181)',
-			contrastText: 'rgb(255 255 255)',
-			dark: 'rgb(234 88 12)',
-			light: 'rgb(251 146 60)',
-		},
-		error: {
-			main: 'rgb(255 0 0)',
-		},
-		background: {
-			default: 'rgb(255 255 255)',
-		},
-		text: {
-			primary: 'rgb(0 0 0)',
-		},
 		custom: {
-			borderColor: grey[200],
-			shadowColor: 'rgb(107 107  107 / 65%)',
-			hoverColor: grey[200],
-			fontColor: grey[100],
+			primary: {
+				default: {
+					main: 'rgb(63 81 181)',
+					contrast: 'rgb(255 255 255)',
+				},
+				light: {
+					main: 'rgb(251 146 60)',
+					contrast: 'rgb(0 0 0)',
+				},
+				dark: {
+					main: 'rgb(234 88 12)',
+					contrast: 'rgb(255 255 255)',
+				},
+			},
+			background: {
+				main: 'rgb(255 255 255)',
+				contrast: 'rgb(0 0 0)',
+			},
+			border: 'rgb(0 0 0 / 15%)',
+			font: 'rgb(0 0 0)',
+			hover: 'rgb(0 0 0 / 15%)',
+			shadow: 'rgb(107 107  107 / 65%)',
 		},
 	},
 });
